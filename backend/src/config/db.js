2c69e79adffd27c984186527061b8e94f675a9f0
Debasish Pradhan <debasishpradhan236@gmail.com>
2026-07-02 18:22:10 +0530

    db updated

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -2,22 +2,20 @@ const { Pool } = require("pg");
 require("dotenv").config();
 
 const pool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT,
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
-console.log(process.env.DB_USER);
-console.log(process.env.DB_PASSWORD);
-console.log(process.env.DB_NAME);
-pool.connect()
+
+pool
+  .connect()
   .then(() => {
     console.log("✅ PostgreSQL Connected");
   })
   .catch((err) => {
     console.log("❌ Database Error");
-    console.log(err.message);
+    console.log(err);
   });
 
 module.exports = pool;
